fc7fad4e5b306d40f4e5a3454b95cbf0f60b7428
Chan Chung <chung.chan@cs.cityu.edu.hk>
2024-07-11 14:07:10 +0800

    updated azure

--- dive_ai/diveai_provider.py
+++ dive_ai/diveai_provider.py
@@ -1,32 +1,39 @@
 from jupyter_ai_magics import BaseProvider
-from jupyter_ai_magics.providers import TextField, EnvAuthStrategy
+from jupyter_ai_magics.providers import EnvAuthStrategy, TextField
 from langchain_openai import ChatOpenAI, AzureChatOpenAI
+from langchain_community.chat_models import ChatOllama
 
-# class ChatDIVEAI(ChatOpenAI):
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs, openai_api_base="http://ai.local-ai/v1/", openai_api_key="123")
+from jupyter_ai_magics import BaseEmbeddingsProvider
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_openai.embeddings import AzureOpenAIEmbeddings
 
-# class ChatDIVEAIProvider(BaseProvider, ChatDIVEAI):
-#     id = "diveai-chat"
-#     name = "AI"
-#     models = [
-#         "mistral",
-#     ]
-#     model_id_key = "model_name"
-#     pypi_package_deps = ["openai"]
-#     # auth_strategy = EnvAuthStrategy(name="OPENAI_API_KEY")
+class DIVEChatOpenAI(ChatOpenAI):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, openai_api_key="123")
 
-class AzureChatDIVEAI(AzureChatOpenAI):
+class DIVEChatOpenAIProvider(BaseProvider, DIVEChatOpenAI):
+    id = "dive-openai"
+    name = " DIVE OpenAI"
+    models = ['*']
+    model_id_key = "model_name"
+    model_id_label = "Model name"
+    pypi_package_deps = ["langchain_openai"]
+    registry = True
+    fields = [
+        TextField(key="openai_api_base", label="Base API URL (required)", format="text"),
+    ]
+    
+class DIVEAzureChatOpenAI(AzureChatOpenAI):
     def __init__(self, **kwargs):
         super().__init__(
             **kwargs,
-            azure_endpoint="https://apim-aoai-eas-dev02.azure-api.net/cs-northcentralus",
+            azure_endpoint="https://apim-aoai-eas-dev02.azure-api.net/cs-eastus",
             openai_api_version="2024-02-01"
         )
 
-class AzureChatDIVEAIProvider(BaseProvider, AzureChatDIVEAI):
-    id = "diveai-azure"
-    name = "AI Azure"
+class DIVEAzureChatOpenAIProvider(BaseProvider, DIVEAzureChatOpenAI):
+    id = "dive-azure"
+    name = " DIVE Azure"
     models = [
         "gpt4o",
     ]
@@ -37,14 +44,10 @@ class AzureChatDIVEAIProvider(BaseProvider, AzureChatDIVEAI):
     auth_strategy = EnvAuthStrategy(
         name="AZURE_OPENAI_API_KEY", keyword_param="openai_api_key"
     )
-        
-class ChatDIVEAI(ChatOpenAI):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs, openai_api_base="http://localhost:11434/v1/", openai_api_key="123")
 
-class ChatDIVEAIProvider(BaseProvider, ChatDIVEAI):
-    id = "diveai"
-    name = "AI"
+class DIVEChatOllamaProvider(BaseProvider, ChatOllama):
+    id = "dive-ollama"
+    name = " DIVE Ollama"
     models = [
         "dolphin-mistral",
         "codestral",
@@ -61,20 +64,12 @@ class ChatDIVEAIProvider(BaseProvider, ChatDIVEAI):
         "llama3",
         "gemma2",
     ]
-    model_id_key = "model_name"
-    pypi_package_deps = ["openai", "langchain_openai"]
-    # auth_strategy = EnvAuthStrategy(name="OPENAI_API_KEY")
+    model_id_key = "model"
+    pypi_package_deps = ["langchain_community"]
 
-
-from jupyter_ai_magics import BaseEmbeddingsProvider
-from langchain_community.embeddings import OllamaEmbeddings
-
-class DIVEAIEmbeddings(OllamaEmbeddings):
-    pass
-
-class DIVEAIEmbeddingsProvider(BaseEmbeddingsProvider, DIVEAIEmbeddings):
-    id = "diveai"
-    name = "AI"
+class DIVEOllamaEmbeddingsProvider(BaseEmbeddingsProvider, OllamaEmbeddings):
+    id = "dive-ollama"
+    name = " DIVE Ollama"
     models = [
         "all-minilm",
         "snowflake-arctic-embed",
@@ -83,3 +78,24 @@ class DIVEAIEmbeddingsProvider(BaseEmbeddingsProvider, DIVEAIEmbeddings):
     ]
     model_id_key = "model"
     pypi_package_deps = ["langchain_community"]
+
+
+class DIVEAzureOpenAIEmbeddings(AzureOpenAIEmbeddings):
+    def __init__(self, **kwargs):
+        super().__init__(
+            **kwargs,
+            azure_endpoint="https://apim-aoai-eas-dev02.azure-api.net/cs-eastus",
+            openai_api_version="2024-02-01"
+        )
+
+class DIVEAzureOpenAIEmbeddingsProvider(BaseEmbeddingsProvider, DIVEAzureOpenAIEmbeddings):
+    id = "dive-azure"
+    name = " DIVE Azure"
+    models = [
+        "text-embedding-3-large"
+    ]
+    model_id_key = "model"
+    pypi_package_deps = ["langchain_openai"]
+    auth_strategy = EnvAuthStrategy(
+        name="AZURE_OPENAI_API_KEY", keyword_param="openai_api_key"
+    )
